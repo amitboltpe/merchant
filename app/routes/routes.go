dd04a1e3d63ecedfae4eb6b457550c580fbdba6a
Amit Jain <amitjain@Mac.lan>
2025-11-10 12:43:55 +0530

    add

--- app/routes/routes.go
+++ app/routes/routes.go
@@ -3,11 +3,10 @@ package routes
 func RegisterRoutes(router *gin.Engine) {
 	//	userController := controllers.NewUserController()
 
-	//api := router.Group("/api/v1")
-	api := router.gin
+	routes := router.Group("/api/v1")
 	{
 		// User routes
-		api.GET("/users", userController.GetUsers)
-		api.POST("/users", userController.CreateUser)
+		//routes.GET("/users", controllers.GetUsers)
+		routes.POST("/users", controllers.GetUsers)
 	}
 }
